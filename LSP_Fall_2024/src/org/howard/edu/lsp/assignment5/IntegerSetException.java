4081343cf40958d0b5f5152957936dedefca1fbe
Rista Subedi <rsubedi536@gmail.com>
2024-10-22 23:54:27 -0400

    Implement a class, IntegerSet, that only contains integers given the specification.

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment5/IntegerSetException.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.assignment5;
+
+/**
+ * {@summary class to throw IntegerSetException}
+ * @author Rista Subedi
+ */
+public class IntegerSetException extends Exception {
+		private static final long serialVersionUID = 1L;
+    public IntegerSetException(String message) {
+        super(message);
+    }
+}
